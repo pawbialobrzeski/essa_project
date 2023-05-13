4f86daaad8a8b763e6e9badef0c3abee331852cd
Michał Strus <michal.strus@interia.pl>
2023-05-13 20:50:56 +0200

    Adding index.html and modifing views.py and urls.py

--- Projekt/Projekt/urls.py
+++ Projekt/Projekt/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import index
 urlpatterns = [
+    path('', index, name = 'index'),
     path('admin/', admin.site.urls),
 ]
