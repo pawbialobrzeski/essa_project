3104f62dbc1b1d1cab98c967d22907a211ebd484
Michał Szydlik <workmich2000@gmail.com>
2023-05-14 23:53:48 +0200

    deletion

--- Projekt/item/views.py
+++ Projekt/item/views.py
@@ -29,3 +29,8 @@ def new(request):
         'form': form,
         'title': 'New item',
     })
+@login_required
+def delete(request, pk):
+    items = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    return redirect('dashboard:index')
