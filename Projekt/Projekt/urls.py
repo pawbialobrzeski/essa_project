ba603e920e2876ac77186673727260ed3303e829
Michał Szydlik <workmich2000@gmail.com>
2023-05-14 20:03:32 +0200

    signup-1

--- Projekt/Projekt/urls.py
+++ Projekt/Projekt/urls.py
@@ -2,10 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
+
 urlpatterns = [
-    path('', index, name = 'index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Projekt/core/views.py
+++ Projekt/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -11,3 +12,10 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
