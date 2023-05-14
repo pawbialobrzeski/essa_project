798b88ae0f7acf3b1ee649b596b2689b102e1d21
Michał Szydlik <workmich2000@gmail.com>
2023-05-14 21:04:07 +0200

    login

--- Projekt/core/forms.py
+++ Projekt/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
@@ -24,5 +24,16 @@ class SignupForm(UserCreationForm):
     
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
     'placeholder': 'Repeat password',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+    'placeholder': 'Username',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Password',
     'class': 'w-full py-4 px-6 rounded-xl'
     }))
