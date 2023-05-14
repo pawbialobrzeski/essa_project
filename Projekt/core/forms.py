7f0a4f7c4e9030974f4f8006f57b339371e34a01
Michał Szydlik <workmich2000@gmail.com>
2023-05-14 20:40:29 +0200

    signup-2

--- Projekt/core/forms.py
+++ Projekt/core/forms.py
@@ -6,3 +6,23 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={
+    'placeholder': 'Username',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+    'placeholder': 'Email',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Password',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Repeat password',
+    'class': 'w-full py-4 px-6 rounded-xl'
+    }))
--- Projekt/core/views.py
+++ Projekt/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
 
@@ -14,7 +14,14 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
     
     return render(request, 'core/signup.html', {
         'form': form
