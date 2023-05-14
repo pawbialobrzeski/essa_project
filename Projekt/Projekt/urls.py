f0a17cafd0177a843e1e7562ea3435fd53d12efc
Michał Strus <michal.strus@interia.pl>
2023-05-14 12:07:09 +0200

    Adding items and showing them on page

--- Projekt/Projekt/urls.py
+++ Projekt/Projekt/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from core.views import index, contact
@@ -5,4 +7,4 @@ urlpatterns = [
     path('', index, name = 'index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Projekt/item/models.py
+++ Projekt/item/models.py
@@ -20,4 +20,7 @@ class Item(models.Model):
     image = models.ImageField(upload_to='item_images', blank= True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_new_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
