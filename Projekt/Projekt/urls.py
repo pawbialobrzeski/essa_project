7f72dcde9bb296de38111d9cd9a336c740af1d12
Michał Strus <michal.strus@interia.pl>
2023-05-14 12:35:06 +0200

    Making related_items and showing all items details

--- Projekt/Projekt/urls.py
+++ Projekt/Projekt/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 urlpatterns = [
     path('', index, name = 'index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
